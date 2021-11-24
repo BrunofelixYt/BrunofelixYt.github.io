a98fb7b5f6c697e1e6eacf6c5b084ec06e22f32d
BrunofelixYt <94948791+BrunofelixYt@users.noreply.github.com>
2021-11-24 03:43:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@
 varcanvas= document.getElementById("tela");
 varcontext= canvas.getContext("2d");
 varbtPausa= document.getElementById("btPausa")
+
+var nx= 0; 
+ var ny= 0;
+  var largura= 20; 
+   var distância= 5; 
+    var borda_x, borda_y;
+    
+ 
