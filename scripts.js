b803a63adc0d288d8dc9cf28ce279ae6d0cbf3e0
BrunofelixYt <94948791+BrunofelixYt@users.noreply.github.com>
2021-11-24 03:56:19 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,48 @@ varcanvas= document.getElementById("tela");
 varcontext= canvas.getContext("2d");
 varbtPausa= document.getElementById("btPausa")
 
-var nx= 0; 
+{var nx= 0; 
  var ny= 0;
   var largura= 20; 
    var distância= 5; 
     var borda_x, borda_y;
-    
+}   
+
+function criarCampo{
+
+nx = Math.floor((canvas.width- distancia) / (largura + distancia));
+
+ny = Math.floor((canvas.height - distancia) / (largura + distancia)); borda_xnx* (distancia + largura) + distancia;
+
+borda_x= nx* (distancia + largura) + distancia;
+
+borda_y=ny* (distancia + largura) + distancia;
+
+}
+
+function novojogo() {
+
+btPausa.innerHTML = "Iniciar";
+
+btPausa.disabled = false;
+
+desenhar();
+
+}
+
+function desenho() {
+
+ var xi, yi;
+
+context.clearRect(0, 0, canvas.width, canvas.height); 
+
+//Desenho de bordas
+
+context.fillStyle="#888888";
+
+context.fillRect(borda_x, 0, canvas.width - 1, canvas.height
+
+- 1);
+
+context.fill Rect(0, borda_y, canvas.width - 1, canvas.height - 1); 
  
