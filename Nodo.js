86de08ca462dd00a40da1dffd280876e26f090d0
BrunofelixYt <94948791+BrunofelixYt@users.noreply.github.com>
2021-11-24 03:34:29 -0300

    Update Nodo.js

--- Nodo.js
+++ Nodo.js
@@ -1,1 +1,34 @@
 
+vardcima= 1;
+ var ddireita= 2;
+ var dbaixo= 3;
+ var desquerda= 4;
+
+function Nodo(px, py, dir) {
+
+ var x, y, direc;
+ this.x =px;
+ this.y = py;
+ this.direc = dir;
+ this.Mover = function() {
+ switch (this.direc) { case dcima:
+
+this.y -= 1;
+
+break;
+
+casedbaixo:this.y += 1;
+
+break;
+caseddireita:this.x += 1;
+break;
+
+case desquerda:
+
+this.x -= 1;
+
+break;
+
+    }
+  };
+ }
